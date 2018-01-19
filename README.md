2063c1f90633682dab474c647d92a5da40a7f00f
wongalofonso <alfonsowongsd@gmail.com>
2018-01-19 11:51:36 -0800

    adds initial project files

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a simple caclutor that will take in the total amount due and total amound received and calculate the amount that is needed to be returned. 
+
+npm start
+axios
+webpack
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, '/src'),
+
+  entry: {
+    javascript: './index'
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/dist'),
+  },
+
+  resolve: {
+    alias: {
+      react: path.join(__dirname, 'node_modules', 'react')
+    },
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]',
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader!autoprefixer-loader"
+      }
+    ],
+  },
+};
